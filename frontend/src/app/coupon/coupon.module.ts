eae01d0c2fca4a6b9fe6129e85d4d22e7e8763c2
Ivan Penaloza <ivan1016017@gmail.com>
2021-10-05 20:03:13 -0500

    frontend file created together with some components of the frontend

---
+++ frontend/src/app/coupon/coupon.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CouponComponent } from './coupon.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CouponComponent]
+})
+export class CouponModule { }
