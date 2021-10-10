721bb8e4892237a8dfd993b695a7bba2f350469b
Ivan Penaloza <ivan1016017@gmail.com>
2021-10-10 08:28:54 -0500

    no changes

--- frontend/src/app/vendor/vendor-signin/vendor-signin.component.ts
+++ frontend/src/app/vendor/vendor-signin/vendor-signin.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/f
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { JwtHelperService } from "@auth0/angular-jwt";
+import { VendorService } from '../vendor.service';
+
 
 
 @Component({
@@ -12,9 +14,39 @@ import { JwtHelperService } from "@auth0/angular-jwt";
 })
 export class VendorSigninComponent implements OnInit {
 
-  constructor() { }
+  helper = new JwtHelperService();
+  vendorForm: FormGroup;
+
+
+  constructor(
+    private vendorService: VendorService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.vendorForm = this.formBuilder.group({
+      companyName: ["", [Validators.required, Validators.maxLength(50)]],
+      password: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
+      confirmPassword: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
+      phone: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
+      image: ["", [Validators.required, Validators.maxLength(200), Validators.minLength(4)]],
+      address: ["", [Validators.required, Validators.maxLength(100), Validators.minLength(4)]],
+      zipcode: ["", [Validators.required, Validators.maxLength(100), Validators.minLength(4)]],
+      bio: ["", [Validators.required, Validators.maxLength(100), Validators.minLength(4)]]
+    })
   }
+
+  signVendor(){}
+
+  showError(error: string){
+    this.toastr.error(error, "Error")
+  }
+
+  showSuccess() {
+    this.toastr.success(`Se ha registrado exitosamente`, "Registro exitoso");
+  }
+
 
 }
